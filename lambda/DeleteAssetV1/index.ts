a8cbb4feef91bfd2749d193aedaf1ef579f566d5
Van Eliot Santos <van.eliot.santos@gmail.com>
2026-09-17 01:38:13 +0800

    Feat: Handle S3 object deletion event by deleting the corresponding DynamoDB record
    Initial implementation where a QueryCommand is used due to the sortKey (schema concern)

--- lambda/DeleteAssetV1/index.ts
+++ lambda/DeleteAssetV1/index.ts
@@ -1,17 +1,52 @@
 import { S3Handler } from 'aws-lambda';
 
-import { s3Client } from '../shared/s3Client';
-
 import { docClient } from '../shared/dynamoClient';
+import { QueryCommand, DeleteCommand } from '@aws-sdk/lib-dynamodb';
 
 export const handler: S3Handler = async (event) => {
   for (const record of event.Records) {
-      if (record.s3.bucket.name !== process.env.ASSETS_BUCKET) {
+    if (record.s3.bucket.name !== process.env.ASSETS_BUCKET) {
       // unexpected bucket — ignore or alarm
       // TODO: Refactor later
       console.warn(`[WARN] Bucket is not ${process.env.ASSETS_BUCKET}`)
     }
 
-    // TODO: Continue
+    console.log('DeleteAssetV1', JSON.stringify({ record }, null, 2))
+
+    const key = record.s3.object.key;
+
+    try {
+      const retrievalResult = await docClient.send(new QueryCommand({
+        TableName: process.env.TABLE_NAME!,
+        // TODO: Types
+        KeyConditionExpression: 'asset_key = :key',
+        ExpressionAttributeValues: { ':key': key },
+      }));
+
+      const { Items } = retrievalResult
+
+      if (!Items || !Items?.length) {
+        console.log('result : ' + JSON.stringify(retrievalResult));
+        continue
+      }
+
+      for (const item of Items) {
+        const deletionResult = await docClient.send(new DeleteCommand({
+          TableName: process.env.TABLE_NAME!,
+          Key: {
+            asset_key: item.asset_key,
+            created_at: item.created_at,
+          },
+        }));
+
+        // TODO: Use a custom flag and create logger utils
+        console.log('result : ' + JSON.stringify(deletionResult));
+      }
+
+    } catch (error) {
+      console.error("Error:", error);
+    }
+
   }
+
 };
