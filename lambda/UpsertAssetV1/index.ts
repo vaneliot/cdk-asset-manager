507cb5fbe24bf46d8074a544d89388a326673f54
Van Eliot Santos <van.eliot.santos@gmail.com>
2026-09-16 17:58:44 +0800

    Cleanup: UpsertAssetV1
    Remove stray import; Fix indentation; Remove console.log; Add TODO about logging

--- lambda/UpsertAssetV1/index.ts
+++ lambda/UpsertAssetV1/index.ts
@@ -1,13 +1,11 @@
 import { S3Handler } from 'aws-lambda';
 
-import { s3Client } from '../shared/s3Client';
-
 import { docClient } from '../shared/dynamoClient';
 import { PutCommand } from '@aws-sdk/lib-dynamodb';
 
 export const handler: S3Handler = async (event) => {
   for (const record of event.Records) {
-      if (record.s3.bucket.name !== process.env.ASSETS_BUCKET) {
+    if (record.s3.bucket.name !== process.env.ASSETS_BUCKET) {
       // unexpected bucket — ignore or alarm
       // TODO: Refactor later
       console.warn(`[WARN] Bucket is not ${process.env.ASSETS_BUCKET}; ${record.s3.bucket.name}`)
@@ -26,7 +24,8 @@ export const handler: S3Handler = async (event) => {
 
     try {
       const data = await docClient.send(new PutCommand(params));
-      console.log('result : ' + JSON.stringify(data));
+      // TODO: Use a custom flag and create logger utils
+      // console.log('result : ' + JSON.stringify(data));
     } catch (error) {
       console.error("Error:", error);
     }
